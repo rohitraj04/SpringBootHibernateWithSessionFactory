89cdbe7dedbc27dbe4944d1075c5149211f0220c
Sidharth Das <sidharthdas.1995@gmail.com>
2018-11-02 02:29:46 +0530

    4th Commit

---
+++ SpringBootHibernateSessionFactory/src/main/java/com/sid/model/AllDetails.java
@@ -0,0 +1,29 @@
+package com.sid.model;
+
+public class AllDetails {
+	
+	private String userName;
+	private String title;
+	private String password;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
---
+++ SpringBootHibernateSessionFactory/src/main/java/com/sid/model/User_Security.java
@@ -0,0 +1,32 @@
+package com.sid.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="user_security")
+@Table(name="user_security")
+public class User_Security {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int us;
+	private String userName;
+	private String userPassword;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	
+
+}
